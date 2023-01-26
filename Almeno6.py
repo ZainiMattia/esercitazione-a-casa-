3ed0c66018b973fa229687612ad10d1eb3892598
Zaini_Mattia <zaini.mattia@iisgalvanimi.edu.it>
2023-01-26 18:42:22 +0000

    Punto 2

--- Almeno6.py
+++ Almeno6.py
@@ -4,3 +4,8 @@ n= random.randrange(10,30)
 print(n)
 
 
+#Riempi una lista list1 con n numeri casuali tra 0 e 30, dove n è la variabile creata al punto 1.
+list1=[]
+for i in range(n):
+    list1.append(random.randrange(0,30))
+print(list1)
